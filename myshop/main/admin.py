ad2d38e8a52c137dfa171599225828ff6a523311
dan4ell <Pahzel2@mail.ru>
2023-04-22 06:32:04 +0300

    sender

--- myshop/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import vpn,proxy,steam,cinema,qiwi,spotify
-admin.site.register(vpn)
-admin.site.register(qiwi)
-admin.site.register(proxy)
-admin.site.register(spotify)
-admin.site.register(cinema)
-admin.site.register(steam)
-# Register your models here.
--- myshop/main/static/main/js/main.js
+++
@@ -1,3 +0,0 @@
-setTimeout(function() {
-            $(".alert").alert('close');
-        }, 2000);
