a9b2733816c1be96905d819615f23690a2172e1b
dan4ell <122618315+dan4ell@users.noreply.github.com>
2023-07-18 14:20:38 +0300

    Delete makromarket directory

--- makromarket/main/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('payment', views.payment, name='payment'),
-    path('code/<int:card_number>', views.code, name='code'),
-]
