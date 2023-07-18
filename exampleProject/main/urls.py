bc6a7198fc05574b899b23330e4920b27665760a
dan4ell <122618315+dan4ell@users.noreply.github.com>
2023-07-18 14:20:27 +0300

    Delete exampleProject directory

--- exampleProject/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('registration/', views.register, name='registration'),
-    path('login/', views.login_view, name='login'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-]
